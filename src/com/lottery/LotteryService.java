66f28a587c76d1204387f5359db38cb2b8e6130b
Conor Corcoran <ccorcoran@internal.onaware.com>
2019-08-22 16:28:59 +0100

    All rest calls finsihed

--- src/com/lottery/LotteryService.java
+++ src/com/lottery/LotteryService.java
@@ -1,15 +1,21 @@
 package com.lottery;
 
 import javax.ws.rs.*;
+
+import org.json.JSONException;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/")
 public class LotteryService {
-	
+
 	private static HashMap<Integer, Ticket> tickets = new HashMap<Integer, Ticket>();
 	private static final AtomicInteger count = new AtomicInteger(0);
-	
+
 	@POST
 	@Path("/ticket/createTicket")
 	@Produces("text/plain")
@@ -21,7 +27,7 @@ public class LotteryService {
 		System.out.println(tickets.size());
 		return "New ticket created";
 	}
-	
+
 	@GET
 	@Path("/ticket/listTickets")
 	@Produces("text/plain")
@@ -32,14 +38,15 @@ public class LotteryService {
 		}
 		return str;
 	}
-	
+
 	@GET
 	@Path("/ticket/getLines")
 	@Produces("text/plain")
-	public void getLines() {
-		tickets.get(2).printLines();
+	public String getLines(@QueryParam("ticketNo") int ticketNo) throws JSONException {
+		System.out.println("Ticket no: " + ticketNo);
+		return tickets.get(ticketNo).getLines().toString();
 	}
-	
+
 	@GET
 	@Path("/ticket/getTicket")	
 	public String getTicket(@QueryParam("id") int id){
@@ -53,23 +60,48 @@ public class LotteryService {
 					ticket = "Ticket id: " + id + " does not exist";
 				}
 			}
-	    }	
+		}	
 		return ticket;
 	}
-	
+
 	@PUT
 	@Path("/ticket/amendTicket")
 	@Produces("text/plain")
-	public void amendTicket() {
-		//Add new lines to a ticket
+	public void amendTicket(@QueryParam("id") int id, @QueryParam("numOfLines") int numOfLines) {
+		tickets.get(id).addLines(numOfLines);
 	}
-	
+
 	@PUT
 	@Path("/status/getStatus")
 	@Produces("text/plain")
-	@Consumes("text/plain")
-	public void getStatus() {
-		//check ticket for results
+	public void getStatus(@QueryParam("id") int id) {
+		HashMap<String, Integer> results = new HashMap<String, Integer>();
+		for(int i = 1; i <= tickets.get(id).getLines().length(); i++) {
+			try {
+				System.out.println("Checking status of line " + i);
+				System.out.println(tickets.get(id).getLines().getJSONArray("Line " + i));
+				results.put("Line " + i, tickets.get(id).getResult(tickets.get(id).getLines().getJSONArray("Line " + i)));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Results");
+		System.out.println(results);
+		sortResults(results);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static void sortResults(HashMap<String, Integer> results){
+		Object[] a = results.entrySet().toArray();
+		Arrays.sort(a, new Comparator<Object>() {
+		    public int compare(Object o1, Object o2) {
+		        return ((Map.Entry<String, Integer>) o2).getValue()
+		                   .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+		    }
+		});
+		for (Object e : a) {
+		    System.out.println(((Map.Entry<String, Integer>) e).getKey() + " : "
+		            + ((Map.Entry<String, Integer>) e).getValue());
+		}
+	}
 }
--- src/com/lottery/Ticket.java
+++ src/com/lottery/Ticket.java
@@ -1,6 +1,10 @@
 package com.lottery;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Ticket {
 	private ArrayList<int[]> lines = new ArrayList<int[]>();
@@ -9,7 +13,7 @@ public class Ticket {
 	public Ticket(){
 		this.checked = false;
 		int numOfLines = 3;
-		
+
 		while(numOfLines > 0) {
 			int[] line = new int[3];
 			for(int i = 0; i < 3;i++) {
@@ -23,22 +27,63 @@ public class Ticket {
 	private static int getRandNum(){
 		return (int)(Math.random() * ((2 - 0) + 1)); 
 	}
-	
-	public ArrayList<int[]> getLines(){
-		return lines;
-	}
 
-	public String printLines(){
-
+	public JSONObject getLines(){
+		JSONObject jsonobj = new JSONObject();
 		for(int i = 0; i < lines.size(); i++) {
 			int[] tempArray = new int[3];
-			String str = "";
+			JSONArray lineArr = new JSONArray();
 			tempArray = lines.get(i);
 			for(int j = 0; j < 3; j++) {
-				str += tempArray[j] + " ";
+				lineArr.put(tempArray[j]);
 			}
-			System.out.println(str);
+			try {
+				jsonobj.put("Line " + (i + 1), lineArr);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 		}
-		return "FILL OUT";
+		return jsonobj;
 	}
+
+	public void addLines(int numOfLines) {
+		while(numOfLines > 0) {
+			int[] line = new int[3];
+			for(int i = 0; i < 3;i++) {
+				line[i] = getRandNum();
+			}
+			lines.add(line);
+			numOfLines--;
+		}
+	}
+
+	public int getResult(JSONArray line) {
+		int result = 0;
+		int total = 0;
+		try {
+			total = line.getInt(0) + line.getInt(1) + line.getInt(2);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		if(total != 2) {
+			try {
+				if(line.getInt(0) == line.getInt(1) && line.getInt(1) == line.getInt(2)) {
+					result = 5;
+				}else if(line.getInt(0) != line.getInt(1) && line.getInt(0) != line.getInt(2)) {
+					result = 1;
+				}
+				else {
+					result = 0;
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			result = 10;
+		}
+		return result;
+	}
+	
 }
